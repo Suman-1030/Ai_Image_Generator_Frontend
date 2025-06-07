5a4e7aedafe972f19b5fb37f278b692cbeac7605
Suman Kagithala <kagithalasuman1030@gmail.com>
2025-06-07 19:01:20 +0530

    css//

--- Pages/Data_path.js
+++ Pages/Data_path.js
@@ -1,5 +1,5 @@
 
 
- // export const Api = "http://localhost:5000"
+  export const Api = "http://localhost:5000"
 
-export const Api ='https://ai-image-generator-backend-4a5z.onrender.com'
+// export const Api ='https://ai-image-generator-backend-4a5z.onrender.com'
